cb6c68329d8924137fb55c67f04a3b7c6ac4aed4
Bruno Adam <111141103+forever-prata@users.noreply.github.com>
2023-11-14 09:11:09 -0300

    Compilador Parcial 14/11
    Entrega compilador parcial 14/11

---
+++ compiladorParcial/token.php
@@ -0,0 +1,15 @@
+<?php
+class token
+{
+    // property declaration
+    public $tok;
+    public $valor;
+    public $inicio;
+    public $fim;
+
+    // method declaration
+    public function mostrar() {
+        echo $this->tokenvl;
+    }
+}
+?>
